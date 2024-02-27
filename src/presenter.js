2dc9fc8dd0fd7c15a4ea0be27053a6fdbdeb38ee
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-02-26 23:00:08 -0400

    se implemento interfaz a FizzBuzz

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import generarFizzBuzz from "./fizzbuzz";
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const number = Number.parseInt(first.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + generarFizzBuzz(number) + "</p>";
 });
